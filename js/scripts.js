48a4b356e6423f0d602b41f38ae39416f8324db7
Joshua Rinard <jrinard@hotmail.com>
2016-10-21 10:18:41 -0700

    Add name field and loads confirmation with restart option

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ var trackChoice = function(val1,val2,val3,val4,val5) { //Function passing in val
   var answer = (val1 + val2 + val3 + val4 + val5); // conditional statement by comparing values from user input to predetermined values
   if (answer <= 5) {                                // Then assinging it to an answer
     return("#ruby"); // stops the function and returns value to the caller. makes final track #ruby div
-  } else if (answer === 6){
+  } else if ((answer >= 6)&&(answer <= 10)){
     return("#c");
-  } else if (answer >= 7) {
+  } else if (answer >= 11) {
     return("#design")
   }
 };
@@ -14,6 +14,7 @@ var trackChoice = function(val1,val2,val3,val4,val5) { //Function passing in val
 // Front-end Logic
 $(document).ready(function(){ // Function that waits for the document to load before being ready.
     $("#main form").submit(function(event){
+      var nameInput = $("input#name").val();
       var q1Input = parseInt($("#q1").val()); // Grabbing user input value and changing datatype to integer then assinging it to a variable
       var q2Input = parseInt($("#q2").val());
       var q3Input = parseInt($("#q3").val());
@@ -21,9 +22,12 @@ $(document).ready(function(){ // Function that waits for the document to load be
       var q5Input = parseInt($("#q5").val());
 
       var finaltrack = trackChoice(q1Input, q2Input, q3Input, q4Input, q5Input);// calling the function choice while placing user input. Assigning it to a variable
-      $("#c, #ruby, #design").hide(); // hiding any previous revealed divs
-      $(finaltrack).show(); // revealing the div as a result to the user
+      $("#c, #ruby, #design, #main").hide(); // hiding any previous revealed divs
 
+      $(".name").text(nameInput);// Assigns inputed form name into class
+      $("#results").show();//shows div
+
+      $(finaltrack).show(); // revealing the div as a result to the user
 
     event.preventDefault();// Makes the submit event handler not disapear when pressed.
   });
