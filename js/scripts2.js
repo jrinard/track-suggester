54d6b530cced5cdbfd1fb01aabc2b42a84f8d92d
Joshua Rinard <jrinard@hotmail.com>
2016-10-21 15:29:06 -0700

    refine styles

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,6 +1,9 @@
 // Business Logic
 
+
 var trackChoice = function(val1,val2,val3,val4,val5) { //Function passing in val1-val5 for conditonal statement and assigning the result to a variable
+
+  if (val1 !== 0 || val2 !== 0  || val3 !== 0 || val4 !== 0 || val5 !== 0) {
     var answer = (val1 + val2 + val3 + val4 + val5);
     if (answer <= 5) {                                // Then assinging it to an answer
       return("#ruby"); // stops the function and returns value to the caller. makes final track #ruby div
@@ -9,6 +12,10 @@ var trackChoice = function(val1,val2,val3,val4,val5) { //Function passing in val
     } else if (answer >= 11) {
       return("#design")
     }// if 5 end
+} else {
+  return("#notcompleted");
+  }
+
 };// function end
 
 // Front-end Logic
@@ -26,7 +33,7 @@ $(document).ready(function(){ // Function that waits for the document to load be
       $(".name").text(nameInput);// Assigns inputed form name into class
       $("#results").fadeIn();//shows div
       $(finaltrack).show(); // revealing the div as a result to the user
-      $("#not").show(); // revealing the div as a result to the user
+      $("#notcompleted").show(); // revealing the div as a result to the user
 
     event.preventDefault();// Makes the submit event handler not disapear when pressed.
   });
